7cc2730e7f1c3c32cebdd2ade39250307162d34e
Anton Tikhonov <antontikhonov@Antons-MacBook-Pro.local>
2016-08-12 21:45:04 +0300

    segmented control

--- LogNet/LogNet/RepriceViewController.swift
+++ LogNet/LogNet/RepriceViewController.swift
@@ -8,10 +8,10 @@
 
 import UIKit
 
-class RepriceViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class RepriceViewController: UIViewController {
     
-    @IBOutlet weak var tableView: UITableView!
-
+    var containerViewController:ContainerViewController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,93 +20,32 @@ class RepriceViewController: UIViewController, UITableViewDataSource, UITableVie
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
-        self.addSegmentedControl()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+    // MARK: - IBActions
+
+    @IBAction func switchTableView(sender: AnyObject) {
+        self.containerViewController.switchViewControllers()
+    }
+    
     // MARK: - Methods
-    
-    func addSegmentedControl() {
-        let control = UISegmentedControl(items: ["Pending", "Completed"])
-        control.addTarget(self, action: #selector(self.segmentedControlAction(_:)), forControlEvents: .TouchUpInside)
-    }
 
-    func segmentedControlAction(sender:AnyObject?){
-        
-    }
-    
-    // MARK: - Table view data source
 
-    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
-    }
-
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 10
-    }
-
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("NotificationCell", forIndexPath: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    
-    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        
-    }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        if segue.identifier == "embededContainer" {
+            self.containerViewController = segue.destinationViewController as! ContainerViewController
+        }
     }
-    */
 
 }
