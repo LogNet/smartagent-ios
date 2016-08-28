8e882189352f0c8ce508bab006cfe70c8fdffb1f
Anton Tikhonov <antontikhonov@Antons-MacBook-Pro.local>
2016-08-28 18:57:59 +0300

    Commit

---
+++ LogNet/LogNet/PNRContactCell.swift
@@ -0,0 +1,24 @@
+//
+//  PNRContactCell.swift
+//  SmartAgent
+//
+//  Created by Anton Tikhonov on 8/28/16.
+//  Copyright © 2016 Anton Tikhonov. All rights reserved.
+//
+
+import UIKit
+
+class PNRContactCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
